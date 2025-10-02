a926bce254efd29e54479584c3fa72f78b946c26
daTaHere <adamhuy123@yahoo.com>
2025-10-01 22:24:17 -0700

    clean up commented code

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,30 +1,3 @@
-// // src/api/api.ts
-// const API_URL = import.meta.env.VITE_API_URL; // <--- Your backend URL hear.
-
-// type FetchOptions = RequestInit & { auth?: boolean };
-
-// export async function apiFetch<T>(
-//   endpoint: string,
-//   options: FetchOptions = {}
-// ): Promise<T> {
-//   const headers: HeadersInit = {
-//     'Content-Type': 'application/json',
-//     ...(options.headers || {}),
-//   };
-
-//   const res = await fetch(`${API_URL}${endpoint}`, {
-//     ...options,
-//     headers,
-//   });
-
-//   if (!res.ok) {
-//     throw new Error(`API error: ${res.status}`);
-//   }
-
-//   return res.json() as Promise<T>;
-// }
-
-// src/api/api.ts
 import { mockUsers } from '../data/mockUsers';
 
 type FetchOptions = RequestInit & { auth?: boolean };
